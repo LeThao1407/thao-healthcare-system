17d73566f1b4a3b095e4a69e2afbea05bdb1b8c5
LeThao1407 <lephuongthao14072005@gmail.com>
2025-07-19 16:55:19 +0700

    Add Patient model

---
+++ Patient.java
@@ -0,0 +1,22 @@
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate dateOfBirth;
+}
